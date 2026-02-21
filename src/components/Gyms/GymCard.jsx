ff8be4e43fcf36339e9743012ef43fa2e6529ce5
GonzaloTT <maganagonzalosegovia@gmail.com>
2026-02-20 23:35:44 -0600

    Se finalizo componente GymsCard y se da por concluida la fase de desarrollo UI

--- src/components/Gyms/GymCard.jsx
+++ src/components/Gyms/GymCard.jsx
@@ -3,12 +3,14 @@ import './GymCard.css'
 function GymCard() {
   return (
     <article className="gym-card">
-      <h3 className="gym-card__title">
-        Power Gym Center
-      </h3>
+      <div className="gym-card__top">
+        <h3 className="gym-card__title">
+          Power Gym Center
+        </h3>
 
-      <div className="gym-card__rating">
-        ★ 4.6
+        <div className="gym-card__rating">
+          ★ <span>4.6</span>
+        </div>
       </div>
 
       <p className="gym-card__address">
