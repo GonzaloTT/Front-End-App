df8a60d9e802114351e072b610bad2270a53d3df
GonzaloTT <maganagonzalosegovia@gmail.com>
2026-02-20 22:17:08 -0600

    Se finalizo componente Footer asi concluyendo con el layout principal

--- src/components/Layout/Footer/Footer.jsx
+++ src/components/Layout/Footer/Footer.jsx
@@ -1,12 +1,30 @@
 import './Footer.css'
+import { FaPhone, FaEnvelope } from "react-icons/fa"
 
 function Footer() {
   return (
     <footer className="footer">
       <div className="footer__container">
         <p className="footer__text">
-          © {new Date().getFullYear()} GymFinder. Built with precision.
+          © {new Date().getFullYear()} GymFinder. Todos los derechos Reservados.
         </p>
+        <div className="footer__icons">
+          <a 
+            href=""
+            className="footer__icon"
+            aria-label="Teléfono"
+          >
+            <FaPhone />
+          </a>
+
+          <a 
+            href=""
+            className="footer__icon"
+            aria-label="Correo electrónico"
+          >
+            <FaEnvelope />
+          </a>
+        </div>
       </div>
     </footer>
   )
