796040b7ce54b843a760dfd8c8acc9f30cfe1358
Gonzalo <maganagonzalosegovia@gmail.com>
2026-03-09 10:08:56 -0600

    Se corrigio el deploy con npm run build 1.2.0

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './components/App'
 import { GymProvider } from './contexts/GymContext'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <GymProvider>
-    <BrowserRouter basename="/Front-End-App/">
+    <HashRouter>
     <App />
-    </BrowserRouter>
+    </HashRouter>
     </GymProvider>
   </StrictMode>
 )
